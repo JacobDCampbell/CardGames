5134fc2af9e657abec9f912c2da0668ef8f93602
Jacob Campbell <JacobDCampbell@gmail.com>
2020-06-18 12:59:22 -0700

    Initial add of PlayingCards library
    Initializing the PayingCards library will build a "deck" of CardObjects representing a standard 52 card deck. CardObjects can have almost all of their attributes customized, but at this time that has to be done in the code as we do not yet accept command line arguments nor provide a user interface.
    
    Once initialized, add / remove actions can be performed with the `insert_card` and `pull_card` methods.
    
    The `sort_cards_by_suit` has also been added as the first method for organizing / sorting the deck
    
    Examples have been added for the existing methods.

---
+++ Examples/cards_methods.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+# This file contains POC for all methods exposed by the PlayingCards module.
+#
+# TODO: Separate these into grouped example files that are easier to digest and use.
+import sys
+[sys.path.append(_path) for _path in [
+    './lib', '../lib'
+]]
+
+from PlayingCards import Cards
+
+def main():
+    deck = Cards()
+    card_pos = 46
+
+    # Display Summary of all cards in deck
+    print('--- All Cards ---')
+    list_all_cards(deck)
+    print('\n')
+
+    # List cards, by suit
+    print('--- Cards By Suit ---')
+    list_cards_by_suit(deck)
+    print('\n')
+
+    # Pick a card from the deck by its index
+    print('--- Pull A Card From Deck By Index ---')
+    pulled_card = deck.pull_card(card_pos)
+    print(f'The {pulled_card.long_name} has been pulled from the deck')
+    list_all_cards(deck)
+    print('\n')
+
+    # Insert card to known position of the the deck
+    print('--- Insert Card To Known Location ---')
+    ins_pos = deck.insert_card(pulled_card, card_pos)
+    print(f'The {pulled_card.long_name} has been inserted at position {ins_pos} in the deck')
+    list_all_cards(deck)
+    print('\n')
+
+    # Pick a random card from the deck
+    print('--- Pull A Random Card From Deck ---')
+    rand_card = deck.pull_card()
+    print(f'The {rand_card.long_name} has been pulled from the deck')
+    list_cards_by_suit(deck)
+    print('\n')
+
+    # Insert card to random position of the the deck
+    print('--- Insert Card To Random Location ---')
+    ins_pos = deck.insert_card(rand_card)
+    print(f'The {pulled_card.long_name} has been inserted at position {ins_pos} in the deck')
+    list_all_cards(deck)
+    print('\n')
+
+
+def list_all_cards(deck):
+    for idx in range(0, len(deck.cards)):
+        clr_reset = "\033[0m"
+        if deck.cards[idx].color == "red":
+            clr_code = "\033[38;5;1m\033[48;5;15m"
+        elif deck.cards[idx].color == "black":
+            clr_code = "\033[38;5;0m\033[48;5;15m"
+        else:
+            clr_code = "\033[38;5;55m\033[48;5;15m"
+
+        str_idx = str(idx).ljust(2)
+        face = deck.cards[idx].face
+        l_name = deck.cards[idx].long_name
+        val = deck.cards[idx].value
+
+        print(f'{str_idx}| {clr_code}{face.rjust(3)}{clr_reset} | {l_name} [value: {val}]')
+
+def list_cards_by_suit(deck):
+    sorted_deck = deck.sort_cards_by_suit()
+    for suit in sorted_deck:
+        print(f'{suit} : {", ".join(card.rank for card in sorted_deck[suit])}')
+
+
+if __name__ == '__main__':
+    main()
---
+++ lib/PlayingCards.py
@@ -0,0 +1,120 @@
+import random
+
+class Cards(object):
+    def __init__(self):
+        self.numeric_cards = list(range(2,11))
+        self.face_cards = ["jack", "queen", "king", "ace"]
+        self.cards = []
+
+        for _suit in ["hearts", "diamonds", "spades", "clubs"]:
+            for _rank in self.numeric_cards + self.face_cards:
+                self.cards.append(_CardObject(
+                    rank=_rank,
+                    suit=_suit
+                ))
+
+    def sort_cards_by_suit(self):
+        """
+        Arranges self.cards by _CardObject.suit.
+
+        :return: list
+        """
+        ret_val = {}
+        for card in self.cards:
+            if card.suit in ret_val:
+                ret_val[card.suit].append(card)
+            else:
+                ret_val[card.suit] = [card]
+        return ret_val
+
+    def pull_card(self, pos=None):
+        """
+        Returns a _CardObject object from self.deck and removes that _CardObject object from the self.cards list.
+        
+        :param pos: List index position of card to be extracted. If not defined, a random index will be used.
+        :return: _CardObject object
+        """
+        try:
+            if pos is None:
+                pos = self.get_random_card_index()
+            return self.cards.pop(pos)
+        except IndexError:
+            return None
+
+    def insert_card(self, card, pos=None):
+        """
+        Inserts <card> into self.cards list and returns the index of the inserted ojject.
+        
+        :param pos: List index position where <card> is to be inserted. If not defined, a random index will be used.
+        :return: int of inserted index
+        """
+        try:
+            if pos is None:
+                pos = self.get_random_card_index()
+            self.cards.insert(pos, card)
+            return pos
+        except IndexError:
+            self.cards.append(card)
+            return len(self.cards)-1
+
+    def get_random_card_index(self):
+        """
+        Returns the list index for a randomly selected _CardObject object in self.cards
+
+        :return: int
+        """
+        return random.randrange(0, len(self.cards))
+
+
+class _CardObject(object):
+    """
+    Object representing a single card.
+    """
+    def __init__(self, rank, suit, **kwargs):
+        if suit in ["hearts", "diamonds"]:
+            _color = "red"
+        elif suit in ["spades", "clubs"]:
+            _color = "black"
+        else:
+            _color = None
+
+        self.rank = str(rank)
+        self.suit = suit
+        self.rank_abr = kwargs.get("rank_abr", self.rank if isinstance(rank, int) else self.rank[0])
+        self.suit_abr = kwargs.get("suit_abr", self.suit[0])
+        self.color = kwargs.get("color", _color)
+        self.value = kwargs.get("value", self._get_default_value(rank))
+        self.wild = kwargs.get("wild", False)
+        self.short_name = kwargs.get("short_name", f'{self.rank_abr.upper()}{self.suit_abr}')
+        self.long_name = kwargs.get("long_name", f'{self.rank} of {self.suit}')
+        self.name = kwargs.get("name", self.short_name)
+        self.symbol = kwargs.get("symbol", self._get_default_suit_symbol(self.suit))
+        self.face = kwargs.get("face", f'{self.rank_abr.upper()}{self.symbol}')
+
+    def _get_default_value(self, rank):
+        face_card_values = {
+            "jack" : 11,
+            "queen" : 12,
+            "king" : 13,
+            "ace" : 1,
+            "joker" : 0
+        }
+
+        if isinstance(rank, int):
+            return rank
+        elif rank in face_card_values:
+            return face_card_values[rank.lower()]
+        else:
+            return -1
+
+    def _get_default_suit_symbol(self, suit):
+        if suit in ["diamonds", "diamond"]:
+            return "♦"
+        elif suit in ["clubs", "club"]:
+            return "♣"
+        elif suit in ["hearts", "heart"]:
+            return "♥"
+        elif suit in ["spades", "spade"]:
+            return "♠"
+        else:
+            return suit[0].upper()
